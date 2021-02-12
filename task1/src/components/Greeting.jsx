6ed2b9ba1fe5013d474d78e739927cb19149d8c2
Mashnitskaya, Volha <Volha.Mashnitskaya@wolterskluwer.com>
2021-02-12 20:44:18 +0300

    init: init project

---
+++ task1/src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Greeting extends React.PureComponent{
+    render() {
+        return <h1>Hello World!</h1>
+    }
+}
+
+export default Greeting;
---
+++ task1/src/components/Link.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Link = () => <a href="https://reactjs.org/">Link</a> ;
+
+export default Link;
